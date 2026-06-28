007c009642544a7bc54ea1026c56b0293efaaa5a
Soham Borode <sohamborode6@gmail.com>
2026-06-28 18:46:40 +0530

    Retrofit/http error resolved

--- app/src/main/java/com/example/mycoffeeapp/data/remote/RetrofitClient.kt
+++ app/src/main/java/com/example/mycoffeeapp/data/remote/RetrofitClient.kt
@@ -1,17 +1,14 @@
 package com.example.mycoffeeapp.data.remote
 
-import android.content.Context
 import com.example.mycoffeeapp.data.local.SessionManager
 import com.example.mycoffeeapp.data.remote.auth.AuthApiService
 import com.example.mycoffeeapp.data.remote.cart.CartApiService
 import com.example.mycoffeeapp.data.remote.favorite.FavoriteApiService
 import com.example.mycoffeeapp.data.remote.profile.ProfileApiService
-import com.google.gson.Gson
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.create
 
 object RetrofitClient { // determine that only one networking clint exit throughout the lifetime of the application, for memory and conn. res conservation
     private const val BASE_URL = "http://192.168.43.140:8080/"
@@ -35,7 +32,14 @@ object RetrofitClient { // determine that only one networking clint exit through
     private val okHttpClient: OkHttpClient by lazy {
         OkHttpClient.Builder().addInterceptor(authInterceptor).build()
     }
-
+    private val retrofit: Retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+/*
     private fun retrofit(): Retrofit {
         return Retrofit.Builder()
             .baseUrl(BASE_URL)
@@ -43,27 +47,40 @@ object RetrofitClient { // determine that only one networking clint exit through
             .addConverterFactory(GsonConverterFactory.create()) // Convert JSON to kotlin object
             .build()
     }
-
+*/
+    /*
     val authApiService: AuthApiService by lazy {
-        retrofit()
-            .create(AuthApiService::class.java)
+        retrofit().create(AuthApiService::class.java)
     }
     val favoriteApiService: FavoriteApiService by lazy {
-        retrofit()
-            .create(FavoriteApiService::class.java)
+        retrofit().create(FavoriteApiService::class.java)
     }
 
     val profileApiService: ProfileApiService by lazy {
-        retrofit()
-            .create(ProfileApiService::class.java)
+        retrofit().create(ProfileApiService::class.java)
     }
 
     val apiService: CoffeeApiService by lazy {
-        retrofit()
-            .create(CoffeeApiService::class.java)
+        retrofit().create(CoffeeApiService::class.java)
     }
     val cartRemoteApiService: CartApiService by lazy {
-        retrofit()
-            .create(CartApiService::class.java)
+        retrofit().create(CartApiService::class.java)
+    }
+    */
+
+    val authApiService: AuthApiService by lazy {
+        retrofit.create(AuthApiService::class.java)
+    }
+    val favoriteApiService: FavoriteApiService by lazy {
+        retrofit.create(FavoriteApiService::class.java)
+    }
+    val profileApiService: ProfileApiService by lazy {
+        retrofit.create(ProfileApiService::class.java)
+    }
+    val apiService: CoffeeApiService by lazy {
+        retrofit.create(CoffeeApiService::class.java)
+    }
+    val cartRemoteApiService: CartApiService by lazy {
+        retrofit.create(CartApiService::class.java)
     }
 }
